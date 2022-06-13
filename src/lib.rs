40dc85ae4675400b488225e8b1d7d9c22ddf91e0
Luis Osta <luisosta1999@gmail.com>
2022-06-13 15:00:02 -0700

    created basic interface

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+pub trait DidResolver {
+    fn read(&self) -> serde_json::Value;
+    fn create(&self, did: &str, doc: serde_json::Value) -> String;
+}
+
+pub fn create_credential(
+    _cred_type: &str,
+    _cred_subject: serde_json::Value,
+) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    return Err("Not implemented".into());
+}
+
+pub fn create_presentation() -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    return Err("Not implemented".into());
+}
+
+pub fn create_identity(_mnemonic: &str) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    return Err("Not implemented".into());
+}
+
+pub fn create_data_integrity_proof() -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+    return Err("Not implemented".into());
+}
+
+pub fn verify_data_integrity_proof(
+    _doc: serde_json::Value,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    return Err("Not implemented".into());
+}
+
+pub fn verify_presentation(_doc: serde_json::Value) -> Result<bool, Box<dyn std::error::Error>> {
+    return Err("Not implemented".into());
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
