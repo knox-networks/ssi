ea53547c77ab4fc7f549ab8faf6f581c49622f74
Luis Osta <luisosta1999@gmail.com>
2022-06-21 16:47:50 -0700

    refactored protos

--- registry-resolver/src/lib.rs
+++ registry-resolver/src/lib.rs
@@ -1,4 +1,11 @@
-#[path = "../target/proto/registry_api.v1.rs"]
-#[allow(clippy::all)]
-#[rustfmt::skip]
-pub mod registry;
+// #[path = "../target/proto/registry_api.v1.rs"]
+// #[allow(clippy::all)]
+// #[rustfmt::skip]
+// pub mod registry;
+pub mod registry {
+    tonic::include_proto!("registry_api.v1");
+}
+
+pub fn create() {
+    println!("Hello")
+}
