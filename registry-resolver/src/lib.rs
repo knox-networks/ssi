96970f9d2e36d76f07d29c8f3cc2da9754327b8a
Luis Osta <luisosta1999@gmail.com>
2022-06-21 16:54:35 -0700

    created placeholder resolver implementation

--- registry-resolver/src/lib.rs
+++ registry-resolver/src/lib.rs
@@ -1,3 +1,5 @@
+use serde_json::json;
+
 // #[path = "../target/proto/registry_api.v1.rs"]
 // #[allow(clippy::all)]
 // #[rustfmt::skip]
@@ -6,6 +8,14 @@ pub mod registry {
     tonic::include_proto!("registry_api.v1");
 }
 
-pub fn create() {
-    println!("Hello")
+pub struct RegistryResolver {}
+
+impl ssi::DIDResolver for RegistryResolver {
+    fn create(&self, _did: &str, _doc: serde_json::Value) -> String {
+        return String::from("");
+    }
+
+    fn read(&self, _did: &str) -> serde_json::Value {
+        return json!("{}");
+    }
 }
