53747a4cc00b76d91645fb8f7300404b03581d83
Sergey Kudryashov <skudryashov@knox-networks.com>
2022-07-29 10:26:55 -0700

    Remove SSI object state

--- signature/src/keypair.rs
+++ signature/src/keypair.rs
@@ -15,8 +15,6 @@ pub trait KeyPair: core::fmt::Debug + Sized {
 
 #[derive(Debug)]
 pub struct SSIKeyPair {
-    pub(crate) public_key_encoded: Option<String>,
-    pub(crate) private_key_encoded: Option<String>,
     pub(crate) private_key: ed25519_zebra::SigningKey,
     pub(crate) public_key: ed25519_zebra::VerificationKey,
 }
@@ -26,8 +24,6 @@ impl SSIKeyPair {
         let sk = ed25519_zebra::SigningKey::new(rand::thread_rng());
         let vk = ed25519_zebra::VerificationKey::from(&sk);
         return Self {
-            public_key_encoded: None,
-            private_key_encoded: None,
             public_key: vk,
             private_key: sk,
         };
@@ -36,24 +32,10 @@ impl SSIKeyPair {
 
 impl KeyPair for SSIKeyPair {
     fn get_public_key_encoded(&mut self) -> String {
-        match self.public_key_encoded {
-            Some(ref pk) => pk.clone(),
-            None => {
-                let encoded_pk = multibase::encode(multibase::Base::Base58Btc, self.public_key);
-                self.public_key_encoded = Some(encoded_pk.clone());
-                return encoded_pk;
-            }
-        }
+        multibase::encode(multibase::Base::Base58Btc, self.public_key)
     }
     fn get_private_key_encoded(&mut self) -> String {
-        match self.private_key_encoded {
-            Some(ref pk) => pk.clone(),
-            None => {
-                let encoded_pk = multibase::encode(multibase::Base::Base58Btc, self.private_key);
-                self.private_key_encoded = Some(encoded_pk.clone());
-                return encoded_pk;
-            }
-        }
+        multibase::encode(multibase::Base::Base58Btc, self.private_key)
     }
     fn get_public_key(&self) -> ed25519_zebra::VerificationKey {
         return self.public_key.clone();
