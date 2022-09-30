8d9db43ba1a6f0ea0b24c4e8ef719bb542623f1f
Luis Osta <luisosta1999@gmail.com>
2022-09-30 16:01:04 -0700

    58 Implement `From<Ed25519KeyPair>` Trait For `Ed25519DidSigner`  (#59)
    * added from implementation for signer
    
    * moved from implementation for non-referential

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -253,7 +253,11 @@ mod tests {
                 "verification_method":"did:knox:zHRY3o2SDaGrVjLABw3CdderfhiSfVfX1husev7KdSwdU#zHRY3o2SDaGrVjLABw3CdderfhiSfVfX1husev7KdSwdU"},
                 "type":["VerifiableCredential","PermanentResidentCard"]}]});
         // here we test the presentation
-        let signer = signature::suite::ed25519_2020::Ed25519DidSigner::new();
+        let signer: signature::suite::ed25519_2020::Ed25519DidSigner =
+            signature::suite::ed25519_2020::Ed25519KeyPair::new(None)
+                .unwrap()
+                .into();
+
         let (kv_body, kv_subject) = get_body_subject();
 
         let vc = to.create_credential(
