347d8f6a2c84cefae4e083499635794379059bfe
Sergey Kudryashov <iden.82@gmail.com>
2022-10-17 13:35:56 -0700

    Create & Persist Identity As Part Of Wallet Creation In Custodial Wallet#623 (#65)
    SSI part of https://github.com/knox-networks/core/issues/623
    
    Co-authored-by: Sergey Kudryashov <skudryashov@knox-networks.com>
    Co-authored-by: Luis Osta <luisosta1999@gmail.com>

--- registry_resolver/src/lib.rs
+++ registry_resolver/src/lib.rs
@@ -2,7 +2,7 @@ mod registry_client;
 const DID_METHOD: &str = "knox";
 
 #[derive(Clone, Debug)]
-pub struct RegistryResolver<T>
+pub struct RegistryResolver<T = registry_client::GrpcClient>
 where
     T: registry_client::RegistryClient + 'static,
 {
