a782c0a9632421c52dcc7d03803bdde2b87c45dd
Sergey Kudryashov <skudryashov@knox-networks.com>
2022-10-29 00:16:11 +0200

    Test done

--- core/src/credential.rs
+++ core/src/credential.rs
@@ -154,12 +154,11 @@ mod tests {
         });
 
         let res = serde_json::from_str::<VerifiableCredential>(&expect.to_string());
-        println!("{:?}", res.err());
-        // assert!(res.is_ok());
-        // if let Ok(vc) = res {
-        //     let vc = serde_json::to_value(vc).unwrap();
-        //     assert_json_eq!(expect, vc);
-        // }
+        assert!(res.is_ok());
+        if let Ok(vc) = res {
+            let vc = serde_json::to_value(vc).unwrap();
+            assert_json_eq!(expect, vc);
+        }
         Ok(())
     }
 
