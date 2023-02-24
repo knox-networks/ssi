187949ab090a2c5bcff3dd0597d01b3a67cd3cc0
Sergey Kudryashov <skudryashov@knox-networks.com>
2023-02-24 14:49:07 -0800

    Another buf approach

--- registry_resolver/build.rs
+++ registry_resolver/build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let package_path = "buf.build/knox-networks/registry-mgmt";
     let commit = "f7ff6f57030c418e886459a18b35645e";
-    let std::process::Output { status, stderr, .. } = std::process::Command::new("/home/runner/.local/bin/buf")
+    let std::process::Output { status, stderr, .. } = std::process::Command::new("/usr/local/bin/buf")
         .arg("generate")
         .arg(format!("{package_path}:{commit}"))
         .output()?;
