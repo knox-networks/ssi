f21ea78a486da17b1fbb306b0cb3d4d64e273d27
Sergey Kudryashov <skudryashov@knox-networks.com>
2023-03-01 13:21:40 -0800

    Cargo fmt fix

--- registry_resolver/build.rs
+++ registry_resolver/build.rs
@@ -1,18 +1,17 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(not(feature = "ci-ffi-build"))]
-    {
-        let package_path = "buf.build/knox-networks/registry-mgmt";
-        let commit = "f7ff6f57030c418e886459a18b35645e";
-        let std::process::Output { status, stderr, .. } =
-            std::process::Command::new("buf")
-                .arg("generate")
-                .arg(format!("{package_path}:{commit}"))
-                .output()?;
+    // #[cfg(not(feature = "ci-ffi-build"))]
+    // {
+    //     let package_path = "buf.build/knox-networks/registry-mgmt";
+    //     let commit = "f7ff6f57030c418e886459a18b35645e";
+    //     let std::process::Output { status, stderr, .. } = std::process::Command::new("buf")
+    //         .arg("generate")
+    //         .arg(format!("{package_path}:{commit}"))
+    //         .output()?;
 
-        if !status.success() {
-            return Err(String::from_utf8_lossy(&stderr).into());
-        }
-    }
+    //     if !status.success() {
+    //         return Err(String::from_utf8_lossy(&stderr).into());
+    //     }
+    // }
 
     Ok(())
 }
