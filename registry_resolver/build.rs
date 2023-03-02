980d1f2b1472cc6c27754353de37b6e0ac9cf6ae
Sergey Kudryashov <skudryashov@knox-networks.com>
2023-03-02 12:34:46 -0800

    Markers on conditional compilation

--- registry_resolver/build.rs
+++ registry_resolver/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(not(feature = "ci_ffi_build"))]
     {
+        println!("feature -------->>> ci_ffi_build disabled");
         let package_path = "buf.build/knox-networks/registry-mgmt";
         let commit = "f7ff6f57030c418e886459a18b35645e";
         let std::process::Output { status, stderr, .. } = std::process::Command::new("buf")
@@ -16,6 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     #[cfg(feature = "ci_ffi_build")]
     {
+        println!("feature -------->>> ci_ffi_build enabled");
         Ok(())
     }
 }
