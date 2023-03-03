d38f0e3f32df109f35aaecb8d6c7ca96ea7a1b80
Sergey Kudryashov <skudryashov@knox-networks.com>
2023-03-03 00:19:50 -0800

    Build check

--- registry_resolver/build.rs
+++ registry_resolver/build.rs
@@ -1,7 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(not(feature = "ci_ffi_build"))]
     {
-        println!("feature -------->>> ci_ffi_build disabled");
         let package_path = "buf.build/knox-networks/registry-mgmt";
         let commit = "f7ff6f57030c418e886459a18b35645e";
         let std::process::Output { status, stderr, .. } = std::process::Command::new("buf")
@@ -9,15 +8,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .arg(format!("{package_path}:{commit}"))
             .output()?;
 
+            println!("feature status {:?}", status);
         if !status.success() {
             return Err(String::from_utf8_lossy(&stderr).into());
         }
-        Ok(())
     }
-
-    #[cfg(feature = "ci_ffi_build")]
-    {
-        println!("feature -------->>> ci_ffi_build enabled");
-        Ok(())
-    }
+    Ok(())
 }
