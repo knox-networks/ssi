3437e8c11097ff8573e674db3298d55c4c488fb1
Sergey Kudryashov <skudryashov@knox-networks.com>
2023-03-03 00:26:01 -0800

    Cargo fmt

--- registry_resolver/build.rs
+++ registry_resolver/build.rs
@@ -1,17 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[cfg(not(feature = "ci_ffi_build"))]
-    {
-        let package_path = "buf.build/knox-networks/registry-mgmt";
-        let commit = "f7ff6f57030c418e886459a18b35645e";
-        let std::process::Output { status, stderr, .. } = std::process::Command::new("buf")
-            .arg("generate")
-            .arg(format!("{package_path}:{commit}"))
-            .output()?;
+    let package_path = "buf.build/knox-networks/registry-mgmt";
+    let commit = "f7ff6f57030c418e886459a18b35645e";
+    let std::process::Output { status, stderr, .. } = std::process::Command::new("buf")
+        .arg("generate")
+        .arg(format!("{package_path}:{commit}"))
+        .output()?;
 
-            println!("feature status {:?}", status);
-        if !status.success() {
-            return Err(String::from_utf8_lossy(&stderr).into());
-        }
+    println!("feature status {:?}", status);
+    if !status.success() {
+        return Err(String::from_utf8_lossy(&stderr).into());
     }
     Ok(())
 }
