f4ab70f5a2bcb804d30e3cd89afa94181260c780
Luis Osta <luisosta1999@gmail.com>
2023-08-25 15:09:02 -0500

    148 Update SSI Buf Version (#149)

--- registry_resolver/build.rs
+++ registry_resolver/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let package_path = "buf.build/knox-networks/registry-mgmt";
-    let commit = "addbc87cc6424f7ab40534825fa0cb70";
+    let commit = "43801fd1632e40668c5ae75110550d6d";
     let std::process::Output { status, stderr, .. } = std::process::Command::new("buf")
         .arg("generate")
         .arg(format!("{package_path}:{commit}"))
