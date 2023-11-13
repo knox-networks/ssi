1cab991a8ff9c8dc83edf7f0caa7a6299c2da284
Luis Osta <luisosta1999@gmail.com>
2023-11-13 14:37:31 -0600

    started data integrity support

--- core/src/proof.rs
+++ core/src/proof.rs
@@ -1,5 +1,3 @@
-use sha2::{Digest, Sha512};
-
 mod normalization;
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
@@ -16,6 +14,16 @@ pub struct DataIntegrityProof {
     pub proof_value: String,
 }
 
+impl std::fmt::Display for DataIntegrityProof {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{{\"type\": \"{}\", \"created\": \"{}\", \"verificationMethod\": \"{}\", \"proofPurpose\": \"{}\", \"proofValue\": \"{}\"}}",
+            self.proof_type, self.created, self.verification_method, self.proof_purpose, self.proof_value
+        )
+    }
+}
+
 // Use it as an example
 /// Given a JSON-LD document, create a data integrity proof for the document.
 /// Currently, only `Ed25519Signature2018` data integrity proofs in the JSON-LD format can be created.
@@ -24,11 +32,12 @@ pub fn create_data_integrity_proof<S: signature::suite::Signature>(
     doc: serde_json::Value,
     relation: signature::suite::VerificationRelation,
 ) -> Result<DataIntegrityProof, super::error::Error> {
-    let mut hasher = Sha512::new();
-    hasher.update(normalization::normalize(doc));
-    let result = hasher.finalize();
+    let hashed_normalized_doc = normalization::normalize(doc);
 
-    let encoded_sig = signer.encoded_relational_sign(&result, relation)?;
+    println!("result {:?}", hashed_normalized_doc);
+
+    let encoded_sig = signer.encoded_relational_sign(&hashed_normalized_doc, relation)?;
+
     Ok(DataIntegrityProof {
         proof_type: signer.get_proof_type(),
         created: chrono::Utc::now().to_rfc3339(),
@@ -40,7 +49,6 @@ pub fn create_data_integrity_proof<S: signature::suite::Signature>(
 
 #[cfg(test)]
 mod tests {
-    use sha2::Digest;
 
     use super::create_data_integrity_proof;
     use signature::suite::DIDSigner;
@@ -50,16 +58,23 @@ mod tests {
 
     #[rstest::rstest]
     #[case::success(
-        serde_json::Value::default(),
+        create_unverified_credential_doc(),
         signature::suite::VerificationRelation::AssertionMethod
     )]
     fn test_create_data_integrity_proof(
         #[case] doc: serde_json::Value,
         #[case] relation: signature::suite::VerificationRelation,
     ) {
-        let kp =
-            signature::suite::ed25519_2020::Ed25519KeyPair::new(TEST_DID_METHOD.to_string(), None)
-                .unwrap();
+        let phrase = "vague sell team fee cluster poet slush topic beef dish wise enter meat brave question before exhibit purity drill reward awkward plug ice dilemma";
+        let mnemonic = signature::suite::ed25519_2020::Mnemonic {
+            phrase: phrase.to_string(),
+            language: signature::suite::ed25519_2020::MnemonicLanguage::English,
+        };
+        let kp = signature::suite::ed25519_2020::Ed25519KeyPair::new(
+            TEST_DID_METHOD.to_string(),
+            Some(mnemonic),
+        )
+        .unwrap();
         let signer: signature::suite::ed25519_2020::Ed25519DidSigner = kp.clone().into();
         let verifier: signature::suite::ed25519_2020::Ed25519DidVerifier = kp.into();
         let res = create_data_integrity_proof(&signer, doc.clone(), relation);
@@ -67,18 +82,19 @@ mod tests {
         assert!(res.is_ok());
         match res {
             Ok(proof) => {
+                println!("{proof}");
                 assert_eq!(proof.proof_type, signer.get_proof_type());
                 assert_eq!(
                     proof.verification_method,
                     signer.get_verification_method(relation)
                 );
                 assert_eq!(proof.proof_purpose, relation.to_string());
-
-                let mut hasher = sha2::Sha512::new();
-                let encoded = doc.to_string();
-                let result = encoded.into_bytes();
-                hasher.update(result);
-                let comparison = hasher.finalize();
+                let comparison = crate::proof::normalization::normalize(doc);
+                // let mut hasher = sha2::Sha512::new();
+                // let encoded = doc.to_string();
+                // let result = encoded.into_bytes();
+                // hasher.update(result);
+                // let comparison = hasher.finalize();
 
                 assert!(verifier
                     .decoded_relational_verify(&comparison, proof.proof_value, relation)
@@ -86,5 +102,39 @@ mod tests {
             }
             Err(e) => panic!("{e:?}"),
         }
+    }
+    fn create_unverified_credential_doc() -> serde_json::Value {
+        let expect = serde_json::json!({
+                "@context": [
+                  "https://www.w3.org/ns/credentials/v2",
+                  "https://www.w3.org/2018/credentials/examples/v1"
+                ],
+                "credentialSubject": {
+                  "birthCountry": "Bahamas",
+                  "birthDate": "1981-04-01",
+                  "commuterClassification": "C1",
+                  "familyName": "Kim",
+                  "gender": "Male",
+                  "givenName": "Francis",
+                  "id": "did:knox:z6MkoBjc4GfEWrdAXAchrDrjc7LBuTVNXySswadG3apCKy9P",
+                  "image": "data:image/png;base64,iVBORw0KGgo...kJggg==",
+                  "lprCategory": "C09",
+                  "lprNumber": "000-000-204",
+                  "residentSince": "2015-01-01",
+                  "type": [
+                    "PermanentResident",
+                    "Person"
+                  ]
+                },
+                "id": "http://credential_mock:8000/api/credential/z6MkoBjc4GfEWrdAXAchrDrjc7LBuTVNXySswadG3apCKy9P",
+                "issuanceDate": "2022-10-28T19:35:20Z",
+                "issuer": "did:knox:z6Mkv9L4S8FQ3qcu8UqG8NFHt5LKcfzPeLvPJB7uW5vrp3WF",
+                "type": [
+                  "VerifiableCredential",
+                  "PermanentResidentCard"
+                ]
+        });
+
+        expect
     }
 }
--- core/src/proof/normalization.rs
+++ core/src/proof/normalization.rs
@@ -1,4 +1,20 @@
-pub fn normalize(doc: serde_json::Value) -> impl AsRef<[u8]> {
+use sophia::{api::source::QuadSource, c14n::hash::HashFunction, inmem::dataset::FastDataset};
+
+pub fn normalize(doc: serde_json::Value) -> [u8; 32] {
     let encoded = doc.to_string();
-    encoded.into_bytes()
+    println!("{encoded}");
+    let mut dataset = FastDataset::new();
+    sophia::jsonld::parse_str(&encoded)
+        .add_to_dataset(&mut dataset)
+        .unwrap();
+    println!("dataset {:?}", dataset);
+
+    let mut output = Vec::<u8>::new();
+    sophia::c14n::rdfc10::normalize(&dataset, &mut output).unwrap();
+    println!("output {:?}, length: {:?}", output, output.len());
+
+    let mut hasher = sophia::c14n::hash::Sha256::initialize();
+    hasher.update(&output);
+
+    hasher.finalize()
 }
