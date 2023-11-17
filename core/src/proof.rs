049e3562028f003ae77fc24bbdd170db91cf43ea
Luis Osta <luisosta1999@gmail.com>
2023-11-17 13:46:04 -0600

    29 Add Support RDFC-1.0 Normalization In Data Integrity Proofs (#162)
    - Adds RDFC-1.0 normalization support in place of URDNA2015 (as they are
    essentially equivalent for our purposes -
    https://www.w3.org/TR/rdf-canon/#urdna2015) to add complete support for
    legacy `Ed25519SignatureSystem` support
    https://www.w3.org/TR/vc-di-eddsa/#the-ed25519signature2020-suite. In
    future work we'll need to support the new `eddsa-rdfc-2022` and
    `DataIntegrityProof` approach

--- core/src/proof.rs
+++ core/src/proof.rs
@@ -1,4 +1,4 @@
-use sha2::{Digest, Sha512};
+use sophia::c14n::hash::HashFunction;
 
 mod normalization;
 
@@ -16,50 +16,70 @@ pub struct DataIntegrityProof {
     pub proof_value: String,
 }
 
+impl std::fmt::Display for DataIntegrityProof {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{{\"type\": \"{}\", \"created\": \"{}\", \"verificationMethod\": \"{}\", \"proofPurpose\": \"{}\", \"proofValue\": \"{}\"}}",
+            self.proof_type, self.created, self.verification_method, self.proof_purpose, self.proof_value
+        )
+    }
+}
+
 // Use it as an example
 /// Given a JSON-LD document, create a data integrity proof for the document.
-/// Currently, only `Ed25519Signature2018` data integrity proofs in the JSON-LD format can be created.
+/// Currently, only `Ed25519Signature2020` data integrity proofs in the JSON-LD format can be created.
+/// We should move to the new spec approach as `Ed25519Signature2020` is considered legacy
+/// Follows algorithm described in https://www.w3.org/TR/vc-data-integrity/#add-proof
 pub fn create_data_integrity_proof<S: signature::suite::Signature>(
     signer: &impl signature::suite::DIDSigner<S>,
-    doc: serde_json::Value,
+    unsecured_doc: serde_json::Value,
     relation: signature::suite::VerificationRelation,
 ) -> Result<DataIntegrityProof, super::error::Error> {
-    let mut hasher = Sha512::new();
-    hasher.update(normalization::normalize(doc));
-    let result = hasher.finalize();
+    let transformed_data = normalization::create_hashed_normalized_doc(unsecured_doc)?;
+    let mut hasher = sophia::c14n::hash::Sha256::initialize();
+    hasher.update(&transformed_data);
+    let hash_data = hasher.finalize();
+    let proof = signer.encoded_relational_sign(&hash_data, relation)?;
 
-    let encoded_sig = signer.encoded_relational_sign(&result, relation)?;
     Ok(DataIntegrityProof {
         proof_type: signer.get_proof_type(),
         created: chrono::Utc::now().to_rfc3339(),
         verification_method: signer.get_verification_method(relation),
         proof_purpose: relation.to_string(),
-        proof_value: encoded_sig,
+        proof_value: proof,
     })
 }
 
 #[cfg(test)]
 mod tests {
-    use sha2::Digest;
 
     use super::create_data_integrity_proof;
     use signature::suite::DIDSigner;
     use signature::suite::DIDVerifier;
+    use sophia::c14n::hash::HashFunction;
 
     const TEST_DID_METHOD: &str = "knox";
 
     #[rstest::rstest]
     #[case::success(
-        serde_json::Value::default(),
+        create_unverified_credential_doc(),
         signature::suite::VerificationRelation::AssertionMethod
     )]
     fn test_create_data_integrity_proof(
         #[case] doc: serde_json::Value,
         #[case] relation: signature::suite::VerificationRelation,
     ) {
-        let kp =
-            signature::suite::ed25519_2020::Ed25519KeyPair::new(TEST_DID_METHOD.to_string(), None)
-                .unwrap();
+        let phrase = "vague sell team fee cluster poet slush topic beef dish wise enter meat brave question before exhibit purity drill reward awkward plug ice dilemma";
+        let mnemonic = signature::suite::ed25519_2020::Mnemonic {
+            phrase: phrase.to_string(),
+            language: signature::suite::ed25519_2020::MnemonicLanguage::English,
+        };
+        let kp = signature::suite::ed25519_2020::Ed25519KeyPair::new(
+            TEST_DID_METHOD.to_string(),
+            Some(mnemonic),
+        )
+        .unwrap();
         let signer: signature::suite::ed25519_2020::Ed25519DidSigner = kp.clone().into();
         let verifier: signature::suite::ed25519_2020::Ed25519DidVerifier = kp.into();
         let res = create_data_integrity_proof(&signer, doc.clone(), relation);
@@ -73,18 +93,51 @@ mod tests {
                     signer.get_verification_method(relation)
                 );
                 assert_eq!(proof.proof_purpose, relation.to_string());
-
-                let mut hasher = sha2::Sha512::new();
-                let encoded = doc.to_string();
-                let result = encoded.into_bytes();
-                hasher.update(result);
-                let comparison = hasher.finalize();
+                let transformed_data =
+                    crate::proof::normalization::create_hashed_normalized_doc(doc).unwrap();
+                let mut hasher = sophia::c14n::hash::Sha256::initialize();
+                hasher.update(&transformed_data);
+                let hash_data = hasher.finalize();
 
                 assert!(verifier
-                    .decoded_relational_verify(&comparison, proof.proof_value, relation)
+                    .decoded_relational_verify(&hash_data, proof.proof_value, relation)
                     .is_ok());
             }
             Err(e) => panic!("{e:?}"),
         }
+    }
+    fn create_unverified_credential_doc() -> serde_json::Value {
+        let expect = serde_json::json!({
+                "@context": [
+                  "https://www.w3.org/ns/credentials/v2",
+                  "https://www.w3.org/2018/credentials/examples/v1"
+                ],
+                "credentialSubject": {
+                  "birthCountry": "Bahamas",
+                  "birthDate": "1981-04-01",
+                  "commuterClassification": "C1",
+                  "familyName": "Kim",
+                  "gender": "Male",
+                  "givenName": "Francis",
+                  "id": "did:knox:z6MkoBjc4GfEWrdAXAchrDrjc7LBuTVNXySswadG3apCKy9P",
+                  "image": "data:image/png;base64,iVBORw0KGgo...kJggg==",
+                  "lprCategory": "C09",
+                  "lprNumber": "000-000-204",
+                  "residentSince": "2015-01-01",
+                  "type": [
+                    "PermanentResident",
+                    "Person"
+                  ]
+                },
+                "id": "http://credential_mock:8000/api/credential/z6MkoBjc4GfEWrdAXAchrDrjc7LBuTVNXySswadG3apCKy9P",
+                "issuanceDate": "2022-10-28T19:35:20Z",
+                "issuer": "did:knox:z6Mkv9L4S8FQ3qcu8UqG8NFHt5LKcfzPeLvPJB7uW5vrp3WF",
+                "type": [
+                  "VerifiableCredential",
+                  "PermanentResidentCard"
+                ]
+        });
+
+        expect
     }
 }
--- core/src/proof/normalization.rs
+++ core/src/proof/normalization.rs
@@ -1,4 +1,30 @@
-pub fn normalize(doc: serde_json::Value) -> impl AsRef<[u8]> {
+use sophia::{
+    api::{parser::QuadParser, source::QuadSource},
+    inmem::dataset::FastDataset,
+    jsonld::loader::HttpLoader,
+};
+
+pub fn create_hashed_normalized_doc(
+    doc: serde_json::Value,
+) -> Result<Vec<u8>, crate::error::Error> {
     let encoded = doc.to_string();
-    encoded.into_bytes()
+    let mut dataset = FastDataset::new();
+    let loader = sophia::jsonld::loader::HttpLoader::default();
+    let options = sophia::jsonld::JsonLdOptions::<HttpLoader>::default();
+    let parser = sophia::jsonld::parser::JsonLdParser::new_with_options(
+        options.with_document_loader(loader),
+    );
+    parser
+        .parse_str(&encoded)
+        .add_to_dataset(&mut dataset)
+        .map_err(|e| {
+            crate::error::Error::Unknown(format!("Error parsing JSON-LD document: {}", e))
+        })?;
+
+    let mut output = Vec::<u8>::new();
+    sophia::c14n::rdfc10::normalize(&dataset, &mut output).map_err(|e| {
+        crate::error::Error::Unknown(format!("Error normalizing JSON-LD dataset1: {}", e))
+    })?;
+
+    Ok(output)
 }
