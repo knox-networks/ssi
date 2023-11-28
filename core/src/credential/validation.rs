34e90e0a1b04ac8bc7825c51dfd1e3afb7115029
Luis Osta <luisosta1999@gmail.com>
2023-11-28 08:52:27 -0600

    added tests for context validation

--- core/src/credential/validation.rs
+++ core/src/credential/validation.rs
@@ -25,3 +25,42 @@ pub fn credential_context_validation(
         _ => Ok(()),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::super::BASE_CREDENTIAL_CONTEXT;
+    use super::ContextValue;
+
+    #[rstest::rstest]
+    #[case::empty_context(
+        vec![],
+        Err(serde_valid::validation::Error::Custom(
+            "Context must contain at least one URI".to_string()
+        ))
+    )]
+    #[case::first_uri_not_base(
+        vec![super::ContextValue::String("https://www.w3.org/2018/credentials/v2".to_string())],
+        Err(serde_valid::validation::Error::Custom(
+            "The first URI must be https://www.w3.org/2018/credentials/v1, instead found https://www.w3.org/2018/credentials/v2".to_string()
+        ))
+    )]
+    #[case::first_uri_not_string(
+        vec![super::ContextValue::Object(std::collections::HashMap::new())],
+        Err(serde_valid::validation::Error::Custom(
+            "The first URI must be a string".to_string()
+        ))
+    )]
+    #[case::valid_context(
+        vec![super::ContextValue::String(BASE_CREDENTIAL_CONTEXT.to_string())],
+        Ok(())
+    )]
+    fn test_validate_credential_context(
+        #[case] context: Vec<super::ContextValue>,
+        #[case] expected: Result<(), serde_valid::validation::Error>,
+    ) {
+        match super::credential_context_validation(&context) {
+            Ok(_) => assert!(expected.is_ok()),
+            Err(e) => assert_eq!(e.to_string(), expected.unwrap_err().to_string()),
+        }
+    }
+}
