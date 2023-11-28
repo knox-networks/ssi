50cfadbab84a4f8d0da66fe2e2960f0dee99c08e
Luis Osta <luisosta1999@gmail.com>
2023-11-28 10:55:37 -0600

    fixed did linting issue

--- ffi/src/did.rs
+++ ffi/src/did.rs
@@ -1,4 +1,4 @@
-#![allow(non_camel_case_types, clippy::non_canonical_clone_impl)]
+#![allow(clippy::all)]
 use crate::error::{MaybeRustError, Reportable, Try};
 use safer_ffi::prelude::*;
 use signature::suite::KeyPair;
