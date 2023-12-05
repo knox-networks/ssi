4c79e1b0a2f19f96b99bd25cd221a71d3a6d322f
Luis Osta <luisosta1999@gmail.com>
2023-12-05 14:38:21 -0600

    added cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -22,6 +22,12 @@ enum Command {
         #[arg(short, long)]
         input_file: String,
     },
+    TestEd25519Signature2020 {
+        #[arg(short, long)]
+        input_file: String,
+        #[arg(short, long)]
+        key_pair_file: String,
+    },
 }
 
 fn main() {
@@ -39,6 +45,34 @@ fn main() {
         } => {
             let input = std::fs::read_to_string(input_file_path).unwrap();
             let vc = ssi::credential::VerifiablePresentation::from_str(&input).unwrap();
+            println!("{}", vc);
+        }
+        Command::TestEd25519Signature2020 {
+            input_file,
+            key_pair_file,
+        } => {
+            let input = std::fs::read_to_string(input_file).unwrap();
+            let kp_file = std::fs::read_to_string(key_pair_file).unwrap();
+            let kp: serde_json::Value = serde_json::from_str(&kp_file).unwrap();
+            let private_key = kp
+                .get("privateKeyMultibase")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string();
+            let kp = ssi::signature::suite::ed25519_2020::Ed25519KeyPair::from_private_key(
+                "test".to_string(),
+                private_key,
+            )
+            .unwrap();
+            let signer: ssi::signature::suite::ed25519_2020::Ed25519DidSigner = kp.into();
+            let c = ssi::credential::Credential::from_str(&input).unwrap();
+            let vc = c
+                .try_into_verifiable_credential(
+                    &signer,
+                    ssi::signature::suite::VerificationRelation::AssertionMethod,
+                )
+                .unwrap();
             println!("{}", vc);
         }
     }
