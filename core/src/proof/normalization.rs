35ef41a113fb42b901cb01a5644076a68b23076b
Luis Osta <luisosta1999@gmail.com>
2023-12-07 09:20:27 -0600

    added hashing function

--- core/src/proof/normalization.rs
+++ core/src/proof/normalization.rs
@@ -1,12 +1,16 @@
 use sophia::{
     api::{parser::QuadParser, source::QuadSource},
+    c14n::hash::HashFunction,
     inmem::dataset::FastDataset,
     jsonld::loader::HttpLoader,
 };
 
-pub fn create_hashed_normalized_doc(
-    doc: serde_json::Value,
-) -> Result<Vec<u8>, crate::error::Error> {
+/**
+ * This function is used to create a normalized document from a JSON-LD document.
+ * It uses the RDFC10 normalization algorithm. As is required by Ed25519Signature2020 and `eddsa-rdfc-2022`.
+ * It is essentially a very slightly modified version of URDNA2015.
+ */
+pub fn create_normalized_doc(doc: serde_json::Value) -> Result<Vec<u8>, crate::error::Error> {
     let encoded = doc.to_string();
     let mut dataset = FastDataset::new();
     let loader = sophia::jsonld::loader::HttpLoader::default();
@@ -27,4 +31,11 @@ pub fn create_hashed_normalized_doc(
     })?;
 
     Ok(output)
+}
+
+pub fn hash(data: &[u8]) -> Result<[u8; 32], crate::error::Error> {
+    let mut hasher = sophia::c14n::hash::Sha256::initialize();
+    hasher.update(data);
+    let hash_data = hasher.finalize();
+    Ok(hash_data)
 }
